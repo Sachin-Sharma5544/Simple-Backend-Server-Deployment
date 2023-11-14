b98dc75675d4cf3519375c722cb5503a7f030a74
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2023-11-14 17:53:42 +0530

    Root rout added

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ app.get("/admin", (req, res, next) => {
     res.status(200).send({ msg: "admin route connected" });
 });
 
+app.get("/", (req, res, next) => {
+    res.status(200).send({ msg: "root route connected" });
+});
+
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
 });
